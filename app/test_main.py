6e55f60dd4312ace2d7c7450395aa8123d291438
Alexandr Titarenko <nook17n1@gmail.com>
2026-04-16 11:02:36 +0300

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -10,8 +10,21 @@ from app.main import get_human_age
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (27, 28, [2, 2]),
         (28, 28, [3, 2]),
+        (-27, -27, [0, 0]),
         (100, 100, [21, 17]),
     ],)
-def test_get_human_age(cat_age: str, dog_age: str, expected: list) -> None:
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_should_raise_error_if_name_not_exist() -> None:
+    with pytest.raises(KeyError):
+        get_human_age(None, 21)
+
+
+def test_should_raise_error_if_name_is_str() -> None:
+    with pytest.raises(AttributeError):
+        get_human_age(21, "21")
